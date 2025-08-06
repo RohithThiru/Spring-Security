52377799ac05b47f94fb4b6627e27d64c60fb085
Rohith <rohitot@amazon.com>
2025-08-06 10:13:30 +0530

    Bcrypt password encoder is added

--- src/main/java/com/example/student/Services/StudentsService.java
+++ src/main/java/com/example/student/Services/StudentsService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -17,6 +18,7 @@ public class StudentsService implements UserDetailsService {
 
     @Autowired
     public StudentsRepository repo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public List<Students> getStudents(){
         List<Students> studentsList = repo.findAll();
@@ -24,12 +26,13 @@ public class StudentsService implements UserDetailsService {
     }
 
     public void addStudent(Students student) {
+
+        student.setPassword(encoder.encode(student.getPassword()));
         repo.save(student);
     }
 
     @Override
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
-
         Students student = repo.findByname(name);
         if (student == null) {
             throw new UsernameNotFoundException(name);
